8ce4f0e29b1826e2082250559608eca435555cbc
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-03-22 22:26:35 -0700

    adjusting backend link

--- src/apiUrl.js
+++ src/apiUrl.js
@@ -4,7 +4,7 @@ let apiUrl;
 if (window.location.hostname === "localhost") {
   apiUrl = "http://localhost:3000";
 } else {
-  apiUrl = "https://pure-anchorage-47818.herokuapp.com/";
+  apiUrl = "https://pure-anchorage-47818.herokuapp.com";
 }
 
 // export api url
